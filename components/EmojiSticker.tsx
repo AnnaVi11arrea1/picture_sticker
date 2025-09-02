edaf47bcb57183e6bbbdceda5cdfea2cb6b886cf
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2025-09-02 18:23:00 +0000

    stopped at emjoi picker

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -7,34 +7,38 @@ type Props = {
     stickerSource: ImageSourcePropType;
 };
 
-const doubleTap = Gesture.Tap()
-    .numberOfTaps(2)
-    .onStart(() => {
-        if (scaleImage.value !== imageSize * 2) {
-            scaleImage.value = scaleImage.value * 2;
-        } else {
-            scaleImage.value = Math.round(scaleImage.value / 2);
-        }
-    });
-
-const imageStyle = useAnimatedStyle(() => {
-    return {
-        width: withSpring(scaleImage.value),
-        height: withSpring(scaleImage.value),
-    };
-});
+// doubleTap and imageStyle will be defined inside the EmojiSticker component
 
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
     const scaleImage = useSharedValue(imageSize);
 
+    const doubleTap = Gesture.Tap()
+        .numberOfTaps(2)
+        .onStart(() => {
+            if (scaleImage.value !== imageSize * 2) {
+                scaleImage.value = scaleImage.value * 2;
+            } else {
+                scaleImage.value = Math.round(scaleImage.value / 2);
+            }
+        });
+
+    const imageStyle = useAnimatedStyle(() => {
+        return {
+            width: withSpring(scaleImage.value),
+            height: withSpring(scaleImage.value),
+        };
+    });
+
     return (
         <View style={{ top: -350 }}>
-            <Animated.Image
-                source={stickerSource}
-                resizeMode="contain"
-                style={{ width: imageSize, height: imageSize }}
-            />
+            <GestureDetector gesture={doubleTap}>
+                <Animated.Image
+                    source={stickerSource}
+                    resizeMode="contain"
+                    style={imageStyle}
+                />
+            </GestureDetector>
         </View>
     );
 }
